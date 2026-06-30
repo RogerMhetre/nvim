e35d97c5015c6d2ea82ca176946ab30ab5769d23
Roger <rogermhetre18@gmail.com>
2026-06-30 16:33:46 +0530

    switched to lazy.nvim with full LSP setup

---
+++ lua/rogerconf/lazy/colors.lua
@@ -0,0 +1,69 @@
+function ColorMyPencils(color)
+    color = color or "tokyonight"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+    {
+        "erikbackman/brightburn.vim",
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+    },
+
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = false,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_indent_guides = false,
+                inverse = true,
+                contrast = "",
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+        end,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "dark",
+                    floats = "dark",
+                },
+            })
+            ColorMyPencils()
+        end
+    },
+}
